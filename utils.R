23c7ddde315b136db93f6ef9c6276198b222192f
Tomek Gałązka <tomasz.galazka.mp@gmail.com>
2023-10-18 15:00:28 +0200

    Add tables and filters.

--- utils.R
+++ utils.R
@@ -1,3 +1,12 @@
+data_header <- function(store, department) {
+  paste0("Showing data from store ", store, " and department ", department)
+}
+
+subset_data <- function(data, cols) {
+  valid_cols <- intersect(cols, colnames(data))
+  data[, valid_cols]
+}
+
 read_data <- function(store) {
   read.csv(paste0("walmart_", store, ".csv"))
 }
